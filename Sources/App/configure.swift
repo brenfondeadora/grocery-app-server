f4fbce5299b17114c97536e7d5c7a32ba079e40e
Brenda SC <brenda.saavedra@fondeadora.com>
2023-05-19 11:07:26 -0600

    feat: connect postgres database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,15 @@
 import Vapor
+import Fluent
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    // register database
+    app.databases.use(.postgres(hostname: "localhost", username: "postgres", password: "", database: "grocerydb"), as: .psql)
+    
     // register routes
     try routes(app)
 }
