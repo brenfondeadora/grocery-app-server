8fb63252551195fdc4fb23690a1e54d4c34c9db6
brsaca <brsaca@gmail.com>
2023-06-22 12:28:43 -0600

    feat: add deleteGroceryCategory

--- Sources/App/Controllers/GroceryController.swift
+++ Sources/App/Controllers/GroceryController.swift
@@ -23,6 +23,34 @@ class GroceryController: RouteCollection {
         
         // GET: /api/users/:userId/grocery-categories
         api.get("grocery-categories", use: getGroceryCategoriesByUser)
+        
+        // DELETE: /api/users/:userId/grocery-categories/:groceryCategoryId
+        api.delete("grocery-categories", ":groceryCategoryId", use: deleteGroceryCategory)
+    }
+    
+    func deleteGroceryCategory(req: Request) async throws -> GroceryCategoryResponseDTO {
+        
+        // get the userId, groceryCategoryId
+        guard let userId = req.parameters.get("userId", as: UUID.self),
+              let groceryCategoryId = req.parameters.get("groceryCategoryId", as: UUID.self)
+        else {
+            throw Abort(.badRequest)
+        }
+        
+        guard let groceryCategory = try await GroceryCategory.query(on: req.db)
+            .filter(\.$user.$id == userId)
+            .filter(\.$id == groceryCategoryId)
+            .first() else {
+                throw Abort(.notFound)
+            }
+        
+        try await groceryCategory.delete(on: req.db)
+        
+        guard let groceryCategoryResponseDTO = GroceryCategoryResponseDTO(groceryCategory) else {
+            throw Abort(.internalServerError)
+        }
+        
+        return groceryCategoryResponseDTO
     }
     
     func getGroceryCategoriesByUser(req: Request) async throws -> [GroceryCategoryResponseDTO] {
